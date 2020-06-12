27e8a7745e670577b493b3d1e07a9bd1d4a886d9
Hanifan29 <abduhanifan0@gmail.com>
2020-06-12 15:09:53 +0700

    bug

--- app/src/main/java/com/abduhanifan/dicoding/githubuserapp/DetailActivity.kt
+++ app/src/main/java/com/abduhanifan/dicoding/githubuserapp/DetailActivity.kt
@@ -2,11 +2,27 @@ package com.abduhanifan.dicoding.githubuserapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+
+        val listUser: User = intent.getParcelableExtra(EXTRA_DATA)
+
+        tv_username.text = listUser.username
+        tv_name.text = listUser.name
+        tv_company.text = listUser.company
+        tv_location.text = listUser.location
+        tv_repository.text = listUser.repository
+        tv_following.text = listUser.following.toString()
+        tv_follower.text = listUser.follower.toString()
+        img_item_avatar.setImageResource(listUser.avatar!!)
+    }
+
+    companion object {
+        var EXTRA_DATA = "0"
     }
 }
